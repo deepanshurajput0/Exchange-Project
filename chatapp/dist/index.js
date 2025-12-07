435f543485cfc40d7d81212a4400e073891ae6ab
deepanshu verma <deepanshuverma186@gmail.com>
2025-12-08 01:20:21 +0530

    created chat rooms where users can chat

--- chatapp/dist/index.js
+++ chatapp/dist/index.js
@@ -1,9 +1,41 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 const wss = new WebSocketServer({ port: 8080 });
-wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
-        console.log('received message', data.toString());
-        ws.send('hello server' + data);
+const subscriptions = {};
+// "userId":"{
+// ws:"",
+// rooms:[ 
+//   roomId1,
+//   roomId1,        
+// ]
+// }"
+console.log(subscriptions);
+wss.on('connection', function connection(userSocket) {
+    const id = randomId();
+    subscriptions[id] = {
+        ws: userSocket,
+        rooms: [],
+    };
+    userSocket.on('message', function message(data) {
+        const parseMsg = JSON.parse(data);
+        if (parseMsg.type === "SUBSCRIBE") {
+            subscriptions[id]?.rooms.push(parseMsg.room);
+        }
+        if (parseMsg.type === "sendMessage") {
+            const message = parseMsg.message;
+            const roomId = parseMsg.roomId;
+            Object.keys(subscriptions).forEach((userId) => {
+                const sub = subscriptions[userId];
+                if (!sub)
+                    return;
+                const { ws, rooms } = sub;
+                if (rooms.includes(roomId)) {
+                    ws.send(message);
+                }
+            });
+        }
     });
 });
+function randomId() {
+    return Math.random();
+}
 //# sourceMappingURL=index.js.map
--- chatapp/src/index.ts
+++ chatapp/src/index.ts
@@ -1,10 +1,54 @@
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, WebSocket } from 'ws'
 
 const wss = new WebSocketServer({port:8080})
 
-wss.on('connection',function connection(ws){
-     ws.on('message',function message(data){
-        console.log('received message',data.toString())
-        ws.send('hello server'+data)
+
+const subscriptions:{[key:string]:{
+   ws:WebSocket
+   rooms:string[] 
+}} = {
+    
+}
+
+// "userId":"{
+      // ws:"",
+      // rooms:[ 
+      //   roomId1,
+      //   roomId1,        
+   // ]
+// }"
+
+
+console.log(subscriptions)
+
+
+wss.on('connection',function connection(userSocket){
+    const id =  randomId()
+     subscriptions[id] = {
+      ws:userSocket,
+      rooms:[],
+     }
+     userSocket.on('message',function message(data){
+      const parseMsg = JSON.parse(data as unknown as string)
+      if(parseMsg.type === "SUBSCRIBE"){
+           subscriptions[id]?.rooms.push(parseMsg.room)  
+      } 
+      if(parseMsg.type === "sendMessage"){
+         const message = parseMsg.message
+         const roomId = parseMsg.roomId
+         Object.keys(subscriptions).forEach((userId)=>{
+            const sub = subscriptions[userId]
+            if(!sub) return ;
+            const { ws, rooms } = sub;
+            if(rooms.includes(roomId)){
+               ws.send(message)
+            }
+         })
+      }
      })
 })
+
+
+function randomId(){
+   return Math.random()
+}
